003f0045788446121f35e0468f8cafd2f5f38234
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2022-07-15 08:39:54 -0300

    feat: add todos reducer to store

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
+import todosReducer from "../reducers/todos";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    todos: todosReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
